842004e455d14fe2a5c663e98f83124e6121d561
4110064003 <xuanku114nchu@gmail.com>
2026-03-18 10:31:29 +0800

    Update Flask host/port for Render

--- app_final.py
+++ app_final.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
+import os
 
 app = Flask(__name__, template_folder='templates')
 
@@ -155,4 +156,5 @@ def calculate_policy_evaluation():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
